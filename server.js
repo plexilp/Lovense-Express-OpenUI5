19d6d335766c581ca83c986095005590a790ad42
PlexDev <nkremeyer@gmail.com>
2024-05-11 15:54:49 +0200

    sqlite and backend works

--- server.js
+++ server.js
@@ -2,32 +2,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081",
+const config = {
+  bRebuildDatabase: false,
 };
 
-app.use(cors(corsOptions));
+start();
 
-// const db = require("./app/models");
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
+async function start() {
+  const app = express();
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+  var corsOptions = {
+    origin: "http://localhost:8081",
+  };
+  const db = require("./app/models");
+  await db.sequelize.sync({ force: config.bRebuildDatabase }).then((oData) => {
+    if (config.bRebuildDatabase) {
+      console.log(">> Drop and re-sync db.");
+    }
+  });
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors(corsOptions));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to my application." });
-});
+  // parse requests of content-type - application/json
+  app.use(bodyParser.json());
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  // parse requests of content-type - application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // simple route
+  app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my application." });
+  });
+
+  // set port, listen for requests
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}
