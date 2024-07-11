2a86ba7ebbccdc4e85afdff7fe71b4e90eb54b36
PlexDev <nkremeyer@gmail.com>
2024-07-11 22:01:44 +0200

    current changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,3 +8,16 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+// import axios from "axios";
+
+// const result = await axios.post(
+//   "https://api.lovense-api.com/api/basicApi/getToken",
+//   {
+//     token: "{Lovense developer token}",
+//     uid: "{user ID on your application}",
+//     uname: "{user nickname on your application}",
+//   }
+// );
+
+// console.log(result);
