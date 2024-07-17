52e6dc92c21d7e9099271df5ffd3b3defa9d4ccb
PlexDev <nkremeyer@gmail.com>
2024-07-17 22:53:45 +0200

    remove unused code from settings controller

--- frontend/de.plexdev.lovapp/webapp/controller/Settings.controller.js
+++ frontend/de.plexdev.lovapp/webapp/controller/Settings.controller.js
@@ -3,25 +3,6 @@ sap.ui.define(
 	function (BaseController, MessageBox, JSONModel) {
 		"use strict";
 
-		return BaseController.extend("de.plexdev.lovapp.controller.Settings", {
-			sayHello: function () {
-				MessageBox.show("Hello World!");
-			},
-
-			async onPressStop() {
-				const oResult = await this.sendPost("/stopDevice?userId=1", {});
-				console.log(oResult);
-			},
-
-			async onPressTest() {
-				const oResult = await this.sendGet("/test");
-				// const oData = { command: "GetToys" };
-				// const oResult = await this.sendPost("/command", oData).then((x) =>
-				// 	console.log(x)
-				// );
-
-				console.log(oResult);
-			},
-		});
+		return BaseController.extend("de.plexdev.lovapp.controller.Settings", {});
 	}
 );
