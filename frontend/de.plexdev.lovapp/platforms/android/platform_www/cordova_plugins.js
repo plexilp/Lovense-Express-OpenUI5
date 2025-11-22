51c8c1261082556e3a55f06b26a19d933cf4ac65
PlexDev <plexdev@gmail.com>
2025-11-22 17:04:07 +0100

    add Stuff for Cordova

--- frontend/de.plexdev.lovapp/platforms/android/platform_www/cordova_plugins.js
+++ frontend/de.plexdev.lovapp/platforms/android/platform_www/cordova_plugins.js
@@ -244,6 +244,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-media": "5.0.4",
     "cordova-plugin-device": "2.1.0",
     "cordova-plugin-network-information": "3.0.0",
-    "cordova-plugin-statusbar": "4.0.0"
+    "cordova-plugin-statusbar": "4.0.0",
+    "cordova-plugin-vibration": "3.1.1"
   };
 });
